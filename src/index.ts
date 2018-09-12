76a5a1d989d5c4507bbe5c76d15e8e59bd95b783
Joni Katajamäki <joni.katajamaki@gmail.com>
2018-09-12 22:32:18 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+export const map = <A, B>(
+  maybe: A | undefined,
+  mapFn: (value: A) => B
+): B | undefined => (maybe === undefined ? undefined : mapFn(maybe))
+
+export const filter = <A>(
+  maybe: A | undefined,
+  predicate: (value: A) => boolean
+): A | undefined =>
+  maybe === undefined || !predicate(maybe) ? undefined : maybe
+
+export const asNullable = <A>(maybe: A | undefined): A | null =>
+  maybe === undefined ? null : maybe
+
+export const getOrThrow = <A>(maybe: A | undefined): A => {
+  if (maybe === undefined) {
+    throw Error('Value was undefined')
+  }
+  return maybe
+}
+
+export const getOrDefault = <A>(maybe: A | undefined, defaultValue: A): A =>
+  maybe === undefined ? defaultValue : maybe
+
+export const getOrElse = <A>(maybe: A | undefined, makeDefault: () => A): A =>
+  maybe === undefined ? makeDefault() : maybe
+
+export const isDefined = <A>(maybe: A | undefined): maybe is A =>
+  maybe !== undefined
+
+export const isUndefined = <A>(maybe: A | undefined): maybe is undefined =>
+  maybe === undefined
